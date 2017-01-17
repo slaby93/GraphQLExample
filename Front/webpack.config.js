26c60b0623c045ca98201d2664ffbc1d84ce7224
Daniel Słaby <pufek93@gmail.com>
2017-01-17 18:29:12 +0100

    Initial commit

---
+++ Front/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
+
+module.exports = config;
